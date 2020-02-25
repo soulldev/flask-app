8b9e9abe9e64e3f36a4655dd80da810628ebc13a
soulldev <soulldev@gmail.com>
2020-02-25 21:23:25 +0200

    initialized

---
+++ resources/user.py
@@ -0,0 +1,18 @@
+from flask_restful import Resource, reqparse
+from models.user import UserModel
+
+class UserRegister(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('username', type=str, required=True, help="This field can't be left blank")
+    parser.add_argument('password', type=str, required=True, help="This field can't be left blank")
+
+    def post(self):
+        data = UserRegister.parser.parse_args()
+
+        if UserModel.find_by_username(data['username']):
+            return {"message" : "UserModel is already registered."}, 400
+        
+        user = UserModel(**data)
+        user.save_to_db()
+        
+        return {"message" : "UserModel created successfully."}, 201
